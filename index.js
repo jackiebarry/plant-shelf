e962a2b527a64ec94aaf44cb4d4e051ae2417db2
jackiebarry <54484334+jackiebarry@users.noreply.github.com>
2023-06-18 12:25:00 -0230

    adding api key

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const plantButton = document.getElementById('plantSearch')
 plantButton.addEventListener("click", function(e){
     
 })
+
+async function sendApiRequest1(array) {
+    let API_KEY = 'sk-hhUC648f17aaee2f71312'
+    let resoponse = await fetch('https://perenual.com/api/species-list?page=1&key=sk-hhUC648f17aaee2f71312&q=monstera');
+    let data = await Response.json();
+    useApiData(data)
+};
