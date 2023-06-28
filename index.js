3f29281596d86d58459a121b24fcb17b170c736b
jackiebarry <54484334+jackiebarry@users.noreply.github.com>
2023-06-28 19:47:29 -0230

    Co-authored-by: Jillian Breau <jillianSylvera@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const plantButton = document.getElementById('plantSearch')
+let plantData 
 
 plantButton.addEventListener("click", function(e){
-    
+    // we would like to get the data of one plant
+
+    console.log(plantData[29])
 })
 
-async function sendApiRequest1(array) {
+async function sendApiRequest1() {
+    // can make these lets consts
     let API_KEY = 'sk-hhUC648f17aaee2f71312'
-    let resoponse = await fetch('https://perenual.com/api/species-list?page=1&key=sk-hhUC648f17aaee2f71312&q=monstera');
-    let data = await Response.json();
-    useApiData(data)
+    // we will want to loop, and fetch page=1 to page=100. We can store all the results plantData
+    let response = await fetch(`https://perenual.com/api/species-list?page=10&key=${API_KEY}`);
+    let data = await response.json();
+
+    return data.data
 };
+
+
+async function onLoad() {
+    plantData = await sendApiRequest1()
+    console.log(plantData)
+}
+
+onLoad()
